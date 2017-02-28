21e77ea08b5bb2617132ff00d36d162218871d95
Geraldine Chauca <gchaucae@gmail.com>
2017-02-28 16:31:40 -0500

    Segunda versión

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
-function function_name(argument) {
-	// body...
-}
+function masaCorporal() {
+	var nombre = document.getElementById('name').value;
+	var peso = document.getElementById('peso').value;
+	var altura = (document.getElementById('altura').value)/100;
+	var resultado = Math.round(peso/(Math.pow(altura,2)));
+	
+	document.getElementById('name').value = "";
+	document.getElementById('peso').value = "";
+	document.getElementById('altura').value = "";
+	
+	event.preventDefault();
+	if (nombre !="" && peso != "" && altura != "" )
+	{document.getElementById('salida').innerHTML = resultado;}
+	else {document.getElementById('salida').innerHTML = 'Error';}
+
+	};
