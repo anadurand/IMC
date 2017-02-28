015483eeb01e78d6767c3167f3fc04b38c0eed67
Ana Durand <ana.dc1004@gmail.com>
2017-02-28 16:15:50 -0500

    funcion creada

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-function function_name(argument) {
+var peso = prompt("Ingrese su peso");
+var estatura = prompt("Ingrese su estatura en metros");
+
+function function_name(peso, estatura) {
 	// body...
+  var frase = "Su indice de masa corporal es: ";
+
+  alert(frase + (peso/ Math.pow(estatura,2)).toFixed(2));
 }
+
+function_name(peso,estatura);
