4800c5b55844997c35569b6c19f4c65071153bec
Usuario <Usuario>
2017-03-05 00:39:43 -0500

    terminado

--- js/main.js
+++ js/main.js
@@ -1,17 +1,27 @@
 function masaCorporal() {
 	var nombre = document.getElementById('name').value;
 	var peso = document.getElementById('peso').value;
-	var altura = (document.getElementById('altura').value)/100;
-	var resultado = Math.round(peso/(Math.pow(altura,2)));
-	
+	var altura = document.getElementById('altura').value;
+
 	document.getElementById('name').value = "";
 	document.getElementById('peso').value = "";
 	document.getElementById('altura').value = "";
-	
+
 	event.preventDefault();
-	if (nombre !="" && peso != "" && altura != "" )
-	{document.getElementById('salida').innerHTML = resultado;}
-	else {document.getElementById('salida').innerHTML = 'Error';}
+	if ((nombre !="" && peso != "") && altura != "" ){
+		validar(peso,altura);
+	}else{
+		document.getElementById('salida').innerHTML = 'Error, ingresar datos';
+	}
 
-	};
+	}
 
+function validar(peso,altura){
+	if(peso.match("[0-9]") && altura.match("[0-9]")){
+		var resultado = Math.round(peso/(Math.pow(altura/100,2)));
+		document.getElementById('salida').innerHTML = resultado;
+	}else{
+		document.getElementById('salida').innerHTML = 'Error, ingresar datos';
+	}
+
+}
